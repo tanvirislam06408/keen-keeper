bc3d2c4b7d765c18d94038e3ffbf373bfce3e94b
tanvir <mstanvirislam05@gmail.com>
2026-04-14 02:07:30 +0600

    friends details page added

---
+++ src/components/RecentInteractions/RecentInteractions.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const RecentInteractions = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default RecentInteractions;
---
+++ src/pages/FriendDetails/QuicCkIn/QuicCkIn.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import callImg from '../../../assets/call.png'
+import textImg from '../../../assets/text.png'
+import videoImg from '../../../assets/video.png'
+const QuicCkIn = () => {
+    return (
+        <div>
+            <div className=' mt-5 px-4 py-8 rounded-2xl shadow border border-gray-200'>         
+                 <p className='font-medium text-lg'>Quick Check-In</p>
+                <div className="flex gap-4 mt-3">
+                    <div className="px-4  py-8 w-full gap-2.5 rounded-2xl shadow border border-gray-200 flex flex-col items-center justify-center">
+                        <div>
+                            <img src={callImg} alt="" />
+                        </div>
+                        <h1 className='text-lg'>Call</h1>
+                    </div>
+                    <div className="px-4 py-8 w-full gap-2.5 rounded-2xl shadow border border-gray-200 flex flex-col items-center justify-center">
+                        <div>
+                            <img src={textImg} alt="" />
+                        </div>
+                        <h1 className='text-lg'>Text</h1>
+                    </div>
+                    <div className="px-4 py-8 w-full gap-2.5 rounded-2xl shadow border border-gray-200 flex flex-col items-center justify-center">
+                        <div>
+                            <img src={videoImg} alt="" />
+                        </div>
+                        <h1 className='text-lg'>Video</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default QuicCkIn;
