6964a27b4c2882626ba3ef22a1c5f8bc49bda193
tanvir <mstanvirislam05@gmail.com>
2026-04-13 22:42:44 +0600

    add react-icons dependency and update Navbar with navigation links; create Home, TimeLine, and Stats components

--- src/pages/Stats/Stats.jsx
+++ src/pages/Stats/Stats.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Stats = () => {
     return (
         <div>
-            home
+            stats
         </div>
     );
 };
 
-export default Home;
+export default Stats;
